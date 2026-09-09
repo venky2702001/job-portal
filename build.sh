03aae2654409b03430389addda59d3cf63f169ff
K VENKATESH <165770963+venky2702001@users.noreply.github.com>
2026-09-09 15:48:14 +0530

    Add createsuperuser to build script

--- build.sh
+++ build.sh
@@ -6,3 +6,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --noinput || true
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+U = get_user_model()
+U.objects.filter(username=os.environ.get('DJANGO_SUPERUSER_USERNAME')).update(role='admin')
+" || true
