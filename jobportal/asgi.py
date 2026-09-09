fc1d8f26068193324c86e7ba62c3b9183b153d82
K VENKATESH <165770963+venky2702001@users.noreply.github.com>
2026-09-09 12:45:49 +0530

    Fix ASGI http routing

--- jobportal/asgi.py
+++ jobportal/asgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 import django
 from django.core.asgi import get_asgi_application
-from .routing import application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobportal.settings')
 django.setup()
+from .routing import application
--- jobportal/routing.py
+++ jobportal/routing.py
@@ -3,9 +3,11 @@ from channels.auth import AuthMiddlewareStack
 import applications.routing
 import jobs.routing
 import notifications.routing  
-    
+from django.core.asgi import get_asgi_application  
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
+    "http":django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             applications.routing.websocket_urlpatterns +
